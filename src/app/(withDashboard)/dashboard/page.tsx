f122adc2830b7d254a0f8f5f81b71c9a62219228
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-02-08 22:59:41 +0600

    google login , gethub login and logout functionality added

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,29 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  // console.log(session);
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      {session?.user && (
+        <div>
+          <Image
+            src={session?.user?.image as string}
+            alt="profile"
+            height={150}
+            width={150}
+            className="mx-auto mt-10 rounded-full"
+          />
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Logged-in User: {session?.user?.email}
+          </h1>
+        </div>
+      )}
     </div>
   );
 };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
@@ -12,7 +13,7 @@ const LoginPage = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    // formState: { errors },
   } = useForm<FormValues>();
 
   const onSubmit = async (data: FormValues) => {
@@ -65,7 +66,7 @@ const LoginPage = () => {
                 id="password"
                 type="password"
                 {...register("password")}
-                placeholder="Email"
+                placeholder="Password"
                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm  sm:text-sm"
                 required
               />
@@ -94,15 +95,29 @@ const LoginPage = () => {
 
           {/* Social Login Buttons */}
           <div className="flex justify-center gap-4 mt-4">
-            <button className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200">
+            <button
+              className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200"
+              onClick={() =>
+                signIn("google", {
+                  callbackUrl: "http://localhost:3000/dashboard",
+                })
+              }
+            >
               <Image
-                src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+                src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
                 width={30}
                 height={30}
                 alt="Google logo"
               />
             </button>
-            <button className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200">
+            <button
+              className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200"
+              onClick={() =>
+                signIn("github", {
+                  callbackUrl: "http://localhost:3000/dashboard",
+                })
+              }
+            >
               <Image
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
                 width={25}
